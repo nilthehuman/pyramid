db7e5f5b1d455a1671a7fe9d2bc936d7b248555d
Dániel Arató <nil.the.human@gmail.com>
2023-08-14 14:39:50 +0200

    Add first unit tests

---
+++ tests/test_unit.py
@@ -0,0 +1,36 @@
+"""Unit tests to check basic expected behaviors."""
+
+from ..src.pyramid import Paradigm
+
+def test_always_pass():
+    assert True
+
+def test_is_pyramid_both_true():
+    matrix = [ [1, 1, 1, 1, 0],
+               [0, 0, 1, 0, 0],
+               [0, 0, 1, 0, 0],
+               [1, 1, 1, 1, 0],
+               [0, 0, 0, 0, 0] ]
+    para = Paradigm(matrix)
+    assert para.is_pyramid()
+    assert para.is_pyramid_strict()
+
+def test_is_pyramid_both_false():
+    matrix = [ [1, 1, 1, 1, 0],
+               [0, 0, 1, 0, 1],
+               [0, 0, 1, 0, 0],
+               [1, 1, 1, 1, 0],
+               [0, 0, 0, 0, 0] ]
+    para = Paradigm(matrix)
+    assert not para.is_pyramid()
+    assert not para.is_pyramid_strict()
+
+def test_is_pyramid_only_strict_false():
+    matrix = [ [0.6, 1, 1, 1, 0],
+               [0.7, 1, 1, 1, 0],
+               [1.0, 0, 0, 0, 0],
+               [1.0, 0, 0, 0, 0],
+               [0.0, 0, 0, 0, 0] ]
+    para = Paradigm(matrix)
+    assert para.is_pyramid()
+    assert not para.is_pyramid_strict()
