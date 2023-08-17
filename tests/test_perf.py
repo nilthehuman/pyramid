86ae4283e4956c12e8a5e295b3d9d67c0ae30dc3
Dániel Arató <nil.the.human@gmail.com>
2023-08-17 16:30:16 +0200

    Include both positive and negative perf test cases

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -4,16 +4,8 @@ from time import time
 
 from ..src.pyramid import Paradigm
 
-def test_is_pyramid_fast_enough():
+def test_is_pyramid_fast_enough_positive():
     matrix = [ [0.0, 0.1, 0.2, 0.3, 0.4],
-               [0.5, 0.6, 0.7, 0.8, 0.9],
-               [1.0, 0.9, 0.8, 0.7, 0.6],
-               [0.5, 0.4, 0.3, 0.2, 0.1],
-               [0.0, 0.1, 0.2, 0.3, 0.4],
-               [0.5, 0.6, 0.7, 0.8, 0.9],
-               [1.0, 0.9, 0.8, 0.7, 0.6],
-               [0.5, 0.4, 0.3, 0.2, 0.1],
-               [0.0, 0.1, 0.2, 0.3, 0.4],
                [0.5, 0.6, 0.7, 0.8, 0.9],
                [1.0, 0.9, 0.8, 0.7, 0.6],
                [0.5, 0.4, 0.3, 0.2, 0.1],
@@ -27,7 +19,38 @@ def test_is_pyramid_fast_enough():
     elapsed_time = time() - start
     assert elapsed_time < 2
 
-def test_is_pyramid_strict_fast_enough():
+
+def test_is_pyramid_fast_enough_negative():
+    matrix = [ [0.0, 0.7, 0.2, 0.3, 0.1],
+               [0.8, 0.6, 0.7, 0.5, 0.9],
+               [1.0, 0.9, 0.8, 0.7, 0.6],
+               [0.5, 0.4, 0.3, 0.2, 1.0],
+               [0.0, 0.7, 0.2, 0.3, 0.1],
+               [0.8, 0.6, 0.7, 0.5, 0.9],
+               [1.0, 0.9, 0.8, 0.7, 0.6],
+               [0.5, 0.4, 0.3, 0.2, 1.0] ]
+    para = Paradigm(matrix=matrix)
+    start = time()
+    assert not para.is_pyramid()
+    elapsed_time = time() - start
+    assert elapsed_time < 2
+
+
+def test_is_pyramid_strict_fast_enough_positive():
+    matrix = [ [0.0, 0.1, 0.2, 0.3, 0.4],
+               [0.5, 0.6, 0.7, 0.8, 0.9],
+               [0.0, 0.1, 0.2, 0.3, 0.4],
+               [0.5, 0.6, 0.7, 0.8, 0.9],
+               [0.0, 0.1, 0.2, 0.3, 0.4],
+               [0.5, 0.6, 0.7, 0.8, 0.9] ]
+    para = Paradigm(matrix=matrix)
+    start = time()
+    assert para.is_pyramid_strict()
+    elapsed_time = time() - start
+    assert elapsed_time < 2
+
+
+def test_is_pyramid_strict_fast_enough_negative():
     matrix = [ [0.0, 0.1, 0.2, 0.3, 0.4],
                [0.5, 0.6, 0.7, 0.8, 0.9],
                [1.0, 0.9, 0.8, 0.7, 0.6],
