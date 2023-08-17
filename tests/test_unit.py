1ba91b31a5b18a3792aa4772920f86ab888692db
Dániel Arató <nil.the.human@gmail.com>
2023-08-17 16:30:35 +0200

    Unit tests for Paradigm.nudge

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -34,3 +34,21 @@ def test_is_pyramid_only_strict_false():
     para = Paradigm(matrix=matrix)
     assert para.is_pyramid()
     assert not para.is_pyramid_strict()
+
+def test_nudge_once():
+    matrix = [ [0, 0, 0],
+               [0, 0, 0],
+               [0, 0, 0] ]
+    para = Paradigm(matrix=matrix)
+    para.nudge(1, 1, True)
+    assert para[1][1] == 1
+
+def test_nudge_twice():
+    matrix = [ [0, 0, 0],
+               [0, 0, 0],
+               [0, 0, 0] ]
+    para = Paradigm(matrix=matrix)
+    para.nudge(1, 1, True)
+    para.experience += 1
+    para.nudge(1, 1, False)
+    assert para[1][1] == 0.5
